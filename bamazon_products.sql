8829a3d2affa2a227c2916f6a56350e3bf51597c
Vincent Nguyen <vnguyen2@gmail.com>
2016-10-23 14:02:02 -0500

    Database created and products populated

---
+++ bamazon_products.sql
@@ -0,0 +1,33 @@
+USE Bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Rubiks Cube', 'Toys and Games', 14.99, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Monopoly', 'Toys and Games', 11.99, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Kitchen Aid Stand Mixer', 'Houseware', 259.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Mikasa Italian Countryside Highball Glasses', 'Houseware', 29.99, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('EVGA GeForce GTX 1080 FTW', 'Electronics', 679.99, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Epson PowerLite Home Cinema 5040UB 3D', 'Electronics', 2999.99, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Quilted Northern Ultra Plush Toilet Paper, 12 rolls', 'Household Supplies', 25.95, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Bounty Select-a-Size Mega Roll Paper Towels - 12 count', 'Household Supplies', 15.95, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Formula 409 All Purpose Cleaner', 'Cleaning Supplies', 3.25, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Simple Green All-Purpose Cleaner Concentrate, 1 Gal', 'Cleaning Supplies', 8.55, 20);
+
+SELECT * FROM products;
