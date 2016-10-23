39c0e54dfceff5f8b00f99fe7528a6def85c3d36
Vincent Nguyen <vnguyen2@gmail.com>
2016-10-23 14:10:16 -0500

    package json and initialnode js file

---
+++ BamazonCustomer.js
@@ -0,0 +1,8 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root", //Your username
+    password: "", //Your password
+    database: "Bamazon"
+})
