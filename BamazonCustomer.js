eff7ddf63a6a5f7a546b0d25e5dcd0bb0fb1d966
Vincent Nguyen <vnguyen2@gmail.com>
2016-10-23 14:23:34 -0500

    display products in console from results

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -6,3 +8,20 @@ var connection = mysql.createConnection({
     password: "", //Your password
     database: "Bamazon"
 })
+
+
+connection.connect(function(err) {
+    if (err) throw err;
+    printStoreListings();
+})
+
+function printStoreListings(res) {
+	connection.query("SELECT * FROM products", function(err, rows) {
+    console.log("!!!ALL AVAILABLE PRODUCTS FOR SALE!!!");
+        rows.forEach(function(row){
+        console.log("\nProduct: " + row.product_name + "\nLocated in the " + row.department_name + " department." + "\nPrice: $" + row.price + "\nQuantity Left: " + row.stock_quantity);
+        });    
+    });
+}
+
+connection.end();
